53eb747983d38b9e5432c6e944cc586c946b4da8
Lucía de la Granda Prieto <luciadelagranda@gmail.com>
2018-03-13 16:47:11 +0100

    Prueba

---
+++ BeluNco/src/main/java/com/uniovi/entities/User.java
@@ -0,0 +1,90 @@
+package com.uniovi.entities;
+
+import javax.persistence.*;
+
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private long id;
+	@Column(unique=true)
+	private String dni;
+	private String name;
+	private String lastName;
+	
+    private String password;
+    @Transient //Specifies that the property or field is not persistent. 
+    private String passwordConfirm;
+
+
+	public User(String dni, String name, String lastName) {
+		super();
+		this.dni = dni;
+		this.name = name;
+		this.lastName = lastName;
+	}
+	
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+
+	public User() {
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	
+	
+	public String getFullName() {
+		return this.name + " " + this.lastName;
+	}
+
+	
+
+}
