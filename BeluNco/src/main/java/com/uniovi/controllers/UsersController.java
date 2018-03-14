7b503cc77327850067ea85addb27f08958bedf5d
Lucía de la Granda Prieto <luciadelagranda@gmail.com>
2018-03-14 10:17:25 +0100

    Ejercicios 4 y 5

--- BeluNco/src/main/java/com/uniovi/controllers/UsersController.java
+++ BeluNco/src/main/java/com/uniovi/controllers/UsersController.java
@@ -1,8 +1,12 @@
 package com.uniovi.controllers;
 
+import java.security.Principal;
+import java.util.LinkedList;
 
 import org.springframework.beans.factory.annotation.*;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,25 +18,24 @@ import com.uniovi.validators.SignUpFormValidator;
 
 @Controller
 public class UsersController {
-	
 
-	@Autowired 
+	@Autowired
 	private UsersService usersService;
-	
+
 	@Autowired
 	private SecurityService securityService;
-	
+
 	@Autowired
 	private SignUpFormValidator signUpFormValidator;
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Model model) {
 		model.addAttribute("user", new User());
-	    return "signup";
+		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
-	public String signup(@ModelAttribute User user,BindingResult result, Model model) {
+	public String signup(@ModelAttribute User user, BindingResult result, Model model) {
 		signUpFormValidator.validate(user, result);
 		if (result.hasErrors()) {
 			return "signup";
@@ -41,12 +44,25 @@ public class UsersController {
 		securityService.autoLogin(user.getEmail(), user.getPasswordConfirm());
 		return "redirect:";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
-	 return "login";
+		return "login";
 	}
 
+	@RequestMapping("/user/list")
+	public String getListado(Model model, 
+			@RequestParam(value = "", required = false) String searchText, Pageable pageable) {
+		Page<User> users = new PageImpl<User>(new LinkedList<User>());
+		if (searchText != null && !searchText.isEmpty()) {
+			users = usersService.searchByEmailAndName(searchText, pageable);
+		} else {
+			users = usersService.getUsers(pageable);
+		}
 
+		model.addAttribute("usersList", users.getContent());
+		model.addAttribute("page", users);
+		return "user/list";
+	}
 
 }
