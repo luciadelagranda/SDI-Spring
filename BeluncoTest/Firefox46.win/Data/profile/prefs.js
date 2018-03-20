11e59c20db347a970ece2cfbfba679b69734f628
Lucía de la Granda Prieto <luciadelagranda@gmail.com>
2018-03-20 15:47:58 +0100

    Finalizacion proyecto falta la base vacia

--- BeluncoTest/Firefox46.win/Data/profile/prefs.js
+++ BeluncoTest/Firefox46.win/Data/profile/prefs.js
@@ -245,3 +245,7 @@ user_pref("browser.shell.checkDefaultBrowser", false);
 user_pref("browser.shell.checkDefaultBrowser", false);
 
 user_pref("browser.shell.checkDefaultBrowser", false);
+
+user_pref("browser.shell.checkDefaultBrowser", false);
+
+user_pref("browser.shell.checkDefaultBrowser", false);
